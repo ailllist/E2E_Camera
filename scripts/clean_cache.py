bebdf1c4c373f49480d15826ee588ae30f0e0b91
Lab-Labtop-Linux <hojun060534@gmail.com>
2022-07-07 21:49:35 +0900

    fixed multi detecting process
    ooooooooooooooooo

--- scripts/clean_cache.py
+++ scripts/clean_cache.py
@@ -3,3 +3,11 @@ import torch
 
 gc.collect()
 torch.cuda.empty_cache()
+torch.cuda.reset_peak_memory_stats()
+torch.cuda.caching_allocator_alloc(1024)
+# torch.cuda.caching_allocator_alloc(1000000000)
+
+# RuntimeError: CUDA out of memory. Tried to allocate 36.00 MiB (GPU 0; 7.93 GiB total capacity; 130.35 MiB already
+# allocated; 47.06 MiB free; 188.00 MiB reserved in total by PyTorch) If reserved memory is >> allocated memory try
+# setting max_split_size_mb to avoid fragmentation.
+# See documentation for Memory Management and PYTORCH_CUDA_ALLOC_CONF
--- scripts/yolov5/detect_with_yolov5.py
+++ scripts/yolov5/detect_with_yolov5.py
@@ -149,7 +149,7 @@ class YOLOv5:
                 self.pub.publish(camera_msg)
                 self.rate.sleep()
 
-            # print("time : ", time.time()-s_time)
+            print("time : ", time.time()-s_time)
         rospy.spin()
 if __name__ == "__main__":
     YOLOv5()
