c26ae4a1f6882adeb50c1f43f3e4fb1b3366482d
Lab-Labtop-Linux <hojun060534@gmail.com>
2022-07-14 19:19:23 +0900

    changed
    .

--- scripts/signal_recv.py
+++ scripts/signal_recv.py
@@ -10,6 +10,7 @@ def printer(data):
 def listener():
     rospy.init_node('listener', anonymous=True)
     rospy.Subscriber('/data_sender/classes', String, printer)
+    rospy.Subscriber('/data_sender/Filtered_classes', String, printer)
     rospy.spin()
 
 if __name__ == "__main__":
