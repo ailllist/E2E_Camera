9de6fc3f8c01e4fd368d22639eab5915eb82f841
Lab-Labtop-Linux <hojun060534@gmail.com>
2022-07-14 20:40:19 +0900

    add l_hold
    a

--- scripts/Advenced_Camera.py
+++ scripts/Advenced_Camera.py
@@ -10,6 +10,7 @@ from E2E_Camera.msg import camera_data
 """
 
 CLASS_CNT = 10 # default
+L_HOLD = False
 
 try:
     param_list = list(rospy.get_param_names())
@@ -65,7 +66,6 @@ class AdvCam:
             except: # 새로문 물체인 경우
                 self.containing_classes[i] = 0
 
-
     def read_data(self, data):
         self.recv_data = seperator(data.yolov5)
 
@@ -75,7 +75,6 @@ class AdvCam:
         while True:
             if rospy.is_shutdown():
                 break
-            # print("processing....")
             self.class_holder(self.recv_data)
             print(self.containing_classes)
             tmp_list = list(self.containing_classes.keys())
@@ -85,7 +84,39 @@ class AdvCam:
             rate.sleep()
 
 
+def l_holder(classes):
+
+    global L_HOLD
+    if "R" in classes or "O" in classes or "G" in classes: # check Traffic light in msg
+
+        if "LR" in classes:  # make L_hold
+            L_HOLD = True
+
+        if L_HOLD:
+            classes = classes.split("/")
+            global_str = ""
+            for i in classes:
+                tmp = i.split("-")
+                if tmp[0] == "R":
+                    tmp_str = "%s-%s-%s" % ("L"+tmp[0], tmp[1], tmp[2])
+                else:
+                    tmp_str = "-".join(tmp)
+                global_str += "/%s" % tmp_str
+            global_str = global_str[1:]
+
+        else:
+            global_str = classes
+            pass
+
+        return global_str
+
+    else:
+        L_HOLD = False
+        return classes
+
+
 def seperator(classes_str: str) -> list:
+    classes_str = l_holder(classes_str)
     tmp_list = [i.split("-") for i in classes_str.split("/")]
     del tmp_list[len(tmp_list)-1]
     return tmp_list
